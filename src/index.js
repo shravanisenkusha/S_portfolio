c7455caed7b913cf429b5e5aa28341c8f4aa4fef
ParthPatil-04 <parths@senkusha.in>
2025-08-23 23:22:52 +0530

    edited files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/S_portfolio">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
